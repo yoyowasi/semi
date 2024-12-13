9e51a0d84640780c74c4d58f8593e0517f36e310
womwouk <Gmetodol1127@gmail.com>
2024-12-13 17:59:33 +0900

    update : 리엑트로 서버 로그인 테스트(미완성)

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,14 +1,19 @@
-import api from './api';
+import api from 'axios';
 
 // 로그인 함수
 export const login = async (username, password) => {
     try {
-        const response = await api.post('/auth/login', { username, password });
-        const { token } = response.data; // 백엔드에서 받은 JWT 토큰
-        localStorage.setItem('jwtToken', token); // 토큰을 로컬 스토리지에 저장
+        const response = await api.post('http://daelim-semiconductor.duckdns.org:8080/login', { email: "youmin302@gmail.com", password: 1234 });
+        // 서버 응답에서 토큰 추출
+        const { token, message } = response.data;
+
+        // 토큰 저장
+        localStorage.setItem('jwtToken', token);
+
+        console.log(message); // "로그인 성공" 메시지 출력
         return token;
     } catch (error) {
-        console.error('Login failed:', error);
+        console.error('Login failed:', error.response?.data || error.message);
         throw error;
     }
 };
